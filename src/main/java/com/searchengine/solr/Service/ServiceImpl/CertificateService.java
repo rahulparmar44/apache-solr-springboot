c2e447757cffd23fd2fc5c32f8d23f267b96a4ed
Siddhartha Rathour <sirathour@deloitte.com>
2023-02-14 17:14:09 +0530

    added sorting

--- src/main/java/com/searchengine/solr/Service/ServiceImpl/CertificateService.java
+++ src/main/java/com/searchengine/solr/Service/ServiceImpl/CertificateService.java
@@ -41,43 +41,53 @@ public class CertificateService implements ICertificateService {
     }
 
     @Override
-    public List<CountObj> getAllCertificateCount()  {
+    public List<CountObj> getAllCertificateCount() {
         ObjectMapper objectMapper = new ObjectMapper();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<String>(headers);
-        HashMap<String,String> countMap = new HashMap<>();
+        HashMap<String, String> countMap = new HashMap<>();
         ArrayList<CountObj> finalResponse = new ArrayList<>();
-        String url = "http://localhost:8983/solr/certifications/select?fl=count,id&indent=true&q.op=OR&q=id:*/certifiedHashersSummary"+"?"+"&rows=1000&useParams=";
+        String url = "http://localhost:8983/solr/certifications/select?fl=count,id&indent=true&q.op=OR&q=id:*/certifiedHashersSummary" + "?" + "&rows=1000&useParams=";
         ResponseEntity<String> response = restTemplate.exchange(url,
                 HttpMethod.GET, new HttpEntity<String>(headers), String.class);
-        LinkedHashMap<String,String> l = new LinkedHashMap<>();
+        LinkedHashMap<String, String> l = new LinkedHashMap<>();
         JsonNode jsonNode = null;
         try {
-             jsonNode = objectMapper.readTree(response.getBody()).get("response").get("docs");
-        }catch (Exception e){
+            jsonNode = objectMapper.readTree(response.getBody()).get("response").get("docs");
+        } catch (Exception e) {
         }
-        ArrayList<HashMap<HashMap<String,String>,HashMap<String,String>>> arrayList = objectMapper.convertValue(jsonNode, ArrayList.class);
-        for (HashMap c :arrayList){
+        ArrayList<HashMap<HashMap<String, String>, HashMap<String, String>>> arrayList = objectMapper.convertValue(jsonNode, ArrayList.class);
+        for (HashMap c : arrayList) {
             String id = Arrays.stream(c.get("id").toString().split("/")).collect(Collectors.toList()).get(0);
-            String uri = "http://localhost:8983/solr/certifications/select?fl=name&indent=true&q.op=OR&q=id:"+id+"&useParams=";
+            String uri = "http://localhost:8983/solr/certifications/select?fl=name&indent=true&q.op=OR&q=id:" + id + "&useParams=";
             ResponseEntity<String> nameResponse = restTemplate.exchange(uri,
                     HttpMethod.GET, new HttpEntity<String>(headers), String.class);
-            JsonNode jsonNode1 =  null;
+            JsonNode jsonNode1 = null;
             try {
                 jsonNode1 = objectMapper.readTree(nameResponse.getBody()).get("response").get("docs");
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
             }
-            ArrayList<HashMap<String,ArrayList<String>>> names =  objectMapper.convertValue(jsonNode1, ArrayList.class);
-            String count =  objectMapper.convertValue(c.get("count"),ArrayList.class).get(0).toString();
+            ArrayList<HashMap<String, ArrayList<String>>> names = objectMapper.convertValue(jsonNode1, ArrayList.class);
+            String count = objectMapper.convertValue(c.get("count"), ArrayList.class).get(0).toString();
             CountObj countObj = new CountObj();
             countObj.setName(names.get(0).get("name").get(0));
             countObj.setCount(Integer.parseInt(count));
             finalResponse.add(countObj);
         }
+        Collections.sort(finalResponse, new Comparator<CountObj>() {
+            @Override
+            public int compare(CountObj c1, CountObj c2) {
+                if (c1.getCount() < c2.getCount())
+                    return 1;
+                else if (c1.getCount() == c2.getCount())
+                    return 0;
+                else
+                    return -1;
+            }
+        });
         return finalResponse;
     }
-
 
 }
