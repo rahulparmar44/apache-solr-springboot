31d0eabb1639954af7d85ee8bc2ce32722b54190
Thakur, Kashis Kumar <kashthakur@deloitte.com>
2023-02-14 12:27:40 +0530

    added two packages

--- src/main/java/com/searchengine/SolrApplication.java
+++ src/main/java/com/searchengine/SolrApplication.java
@@ -1,14 +1,16 @@
-package com.searchengine.solr;
+package com.searchengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableSolrRepositories(
-        basePackages = "com.searchengine.solr.Repository")
+@EnableSolrRepositories
+@EnableJpaRepositories
 public class SolrApplication {
 
     public static void main(String[] args) {
