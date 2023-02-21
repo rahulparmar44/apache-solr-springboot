e6b66233b65df9870d2587bbff77073eea857c26
Siddhartha Rathour <sirathour@deloitte.com>
2023-02-21 10:59:47 +0530

    refactor

--- src/main/java/com/searchengine/solr/Controller/SolrCertificateController.java
+++ src/main/java/com/searchengine/solr/Controller/SolrCertificateController.java
@@ -2,6 +2,7 @@ package com.searchengine.solr.Controller;
 
 import com.searchengine.solr.Model.Certification.Root;
 import com.searchengine.solr.Service.ServiceImpl.SolrCertificateServiceImpl;
+import com.searchengine.solr.Service.SolrCertificateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +16,10 @@ import java.util.List;
 public class SolrCertificateController {
 
     @Autowired
-    SolrCertificateServiceImpl solrCertificateServiceImpl;
+    SolrCertificateService solrCertificateService;
 
     @RequestMapping("get-certificates-by-name")
     List<Root> getCertificatesByName(@RequestParam String name) throws IOException {
-        return  solrCertificateServiceImpl.getCertificatesByName(name);
+        return  solrCertificateService.getCertificatesByName(name);
     }
 }
